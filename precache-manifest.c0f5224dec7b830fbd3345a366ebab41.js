c9d5ae9f8ac43878dde53632dde6e566774f20ed
andypalmera <andreapalmerita@hotmail.com>
2020-07-29 19:49:06 -0300

    Updates

---
+++ precache-manifest.c0f5224dec7b830fbd3345a366ebab41.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dce0e69ecbcd329cda50ff342fe8204",
+    "url": "/my-porfolio/index.html"
+  },
+  {
+    "revision": "b601b9393a256ccedff8",
+    "url": "/my-porfolio/static/css/main.2b3cd12d.chunk.css"
+  },
+  {
+    "revision": "cc8b6406a00b6650fd89",
+    "url": "/my-porfolio/static/js/2.6f79a407.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-porfolio/static/js/2.6f79a407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b601b9393a256ccedff8",
+    "url": "/my-porfolio/static/js/main.f63e0e67.chunk.js"
+  },
+  {
+    "revision": "9570d5c98cda9a6819e5",
+    "url": "/my-porfolio/static/js/runtime-main.0314c926.js"
+  },
+  {
+    "revision": "0bf75abd87ab63a99cfc405054e7786f",
+    "url": "/my-porfolio/static/media/CV-- ANDREA PALMERA.0bf75abd.pdf"
+  },
+  {
+    "revision": "e05d0ead5fffcced00d1569038193539",
+    "url": "/my-porfolio/static/media/Layout sample-.e05d0ead.jpg"
+  },
+  {
+    "revision": "cd491a7b3358df6621aeeb35238af7dd",
+    "url": "/my-porfolio/static/media/Sales control app-.cd491a7b.jpg"
+  },
+  {
+    "revision": "c4a43eb3654f31d36a039f217eb942e1",
+    "url": "/my-porfolio/static/media/To do list-.c4a43eb3.jpg"
+  },
+  {
+    "revision": "9d6bbfbbdbf951f9b95313be61867feb",
+    "url": "/my-porfolio/static/media/background.9d6bbfbb.jpg"
+  },
+  {
+    "revision": "13dfbe67e99315fd99c43903891655af",
+    "url": "/my-porfolio/static/media/buscador de imagenes-.13dfbe67.jpg"
+  },
+  {
+    "revision": "5b46ee5c627207eea1ded5e5a88b31ae",
+    "url": "/my-porfolio/static/media/lapiz.5b46ee5c.svg"
+  },
+  {
+    "revision": "8b3557eee337c1fe69e66bc7242b620c",
+    "url": "/my-porfolio/static/media/movies search engine-.8b3557ee.jpg"
+  },
+  {
+    "revision": "25232d39630f9abfa5fb37a0c0b3ec1f",
+    "url": "/my-porfolio/static/media/pantalla.25232d39.svg"
+  },
+  {
+    "revision": "035f789791d51db1e426fc409c3736be",
+    "url": "/my-porfolio/static/media/react clock.035f7897.jpg"
+  }
+]);
